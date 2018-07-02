286928154ba3be5f59aeff7901b3ac449a16c508
remo5000 <vigneshshankar@gmail.com>
2018-07-02 10:19:51 +0800

    Add plotting code

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import overpy as op
+from gmplot import gmplot
 
 # Get API
 api = op.Overpass()
 
-# Read custom query from xml file
+# Query
 query = "".join(l for l in open('query.xml'))
 result = api.query(query)
-print(len(result.nodes))
+
+# Generate plot from points
+coord_list = [(float(n.lat), float(n.lon)) for n in result.nodes]
+gmap = gmplot.GoogleMapPlotter(1.3068055, 103.8188261, 100)
+
+# Scatter points
+top_attraction_lats, top_attraction_lons = zip(*coord_list)
+gmap.scatter(top_attraction_lats, top_attraction_lons, '#3B0B39', size=5, marker=False)
+
+# Draw
+gmap.draw("my_map.html")
+

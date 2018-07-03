318494acf9360d679c3a15ad982ec8c8dbf20da7
remo5000 <vigneshshankar@gmail.com>
2018-07-03 12:35:51 +0800

    Comment out rendering

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ result = api.query(query)
 
 # Generate plot from points
 coord_list = [(float(n.lat), float(n.lon)) for n in result.nodes]
-gmap = gmplot.GoogleMapPlotter(1.3068055, 103.8188261, 15)
 
-# Scatter points
-top_attraction_lats, top_attraction_lons = zip(*coord_list)
-gmap.scatter(top_attraction_lats, top_attraction_lons, '#3B0B39', size=5, marker=False)
-
-# Heatmap
-gmap.heatmap(top_attraction_lats, top_attraction_lons)
-
-# Render
-gmap.draw("my_map.html")
+# This part has been left out, as there is no need to visualise.
+### gmap = gmplot.GoogleMapPlotter(1.3068055, 103.8188261, 15)
+### 
+### # Scatter points
+### top_attraction_lats, top_attraction_lons = zip(*coord_list)
+### gmap.scatter(top_attraction_lats, top_attraction_lons, '#3B0B39', size=5, marker=False)
+### 
+### # Heatmap
+### gmap.heatmap(top_attraction_lats, top_attraction_lons)
+### 
+### # Render
+### gmap.draw("my_map.html")
 

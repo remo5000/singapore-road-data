a9d601ad018ec16850c26d20390c3d6f4447a9a3
remo5000 <vigneshshankar@gmail.com>
2018-07-10 15:44:49 +0800

    Add road names to text output

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import sys
 from gmplot import gmplot
 
 class RoadPoint:
-    # lat:float lon:float roadname:string
-    def __init__(self, node_id, lat, lon, road_name):
+    # lat:float lon:float roadname:string lane_count:int
+    def __init__(self, node_id, lat, lon, road_name, lane_count):
         self.node_id = node_id
         self.lat = lat
         self.lon = lon
         self.road_name = road_name
+        self.lane_count = lane_count
 
 # Get API
 api = op.Overpass()
@@ -19,15 +20,18 @@ result = api.query(query)
 
 # Get ways (roads or part of roads) with road names 
 all_ways = result.get_ways()
-ways = [w for w in all_ways if 'name' in w.tags]
+ways = [w for w in all_ways if 'name' in w.tags and 'lanes' in w.tags]
 d = {}
 for way in ways:
     for node in way.nodes:
-        rp = RoadPoint(node.id, float(node.lat), float(node.lon), way.tags.get('name')) 
+        rp = RoadPoint(node.id, float(node.lat), float(node.lon), way.tags.get('name'), way.tags.get('lanes')) 
         d[node.id] = rp
+
 road_points = list(d.values())
+# Clear file
+open("coordinates.txt", "w")
 for rp in road_points:
-    print(f'Lat: {rp.lat} Lon: {rp.lon} Road name: {rp.road_name}.', file=open("coordinates.txt", "a"))
+    print(f'Lat: {rp.lat}, Lon: {rp.lon}, Road name: {rp.road_name}, Number of lanes: {rp.lane_count}', file=open("coordinates.txt", "a+"))
 
 # Generate pure coordinates
 coord_list = [(float(rp.lat), float(rp.lon)) for rp in road_points]

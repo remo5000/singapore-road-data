85e6d58f316a91317747c0505173fb608b123fbb
remo5000 <vigneshshankar@gmail.com>
2018-07-10 16:03:33 +0800

    Abstract gmap output

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import overpy as op
 import sys
-from gmplot import gmplot
+
 from road_point import RoadPoint
 from output_text import export_road_points
+from output_scatter import make_gmap
 
 # Get API
 api = op.Overpass()
@@ -23,20 +24,4 @@ for way in ways:
 road_points = list(d.values())
 
 export_road_points(road_points)
-
-# Generate pure coordinates
-coord_list = [(float(rp.lat), float(rp.lon)) for rp in road_points]
-
-# Setting up the canvas size for the map
-gmap = gmplot.GoogleMapPlotter(1.3068055, 103.8188261, 15)
-
-# Scatter points
-top_attraction_lats, top_attraction_lons = zip(*coord_list)
-gmap.scatter(top_attraction_lats, top_attraction_lons, '#3B0B39', size=5, marker=False)
-
-# Heatmap
-gmap.heatmap(top_attraction_lats, top_attraction_lons)
-
-# Render
-gmap.draw("my_map.html")
-
+make_gmap(road_points)

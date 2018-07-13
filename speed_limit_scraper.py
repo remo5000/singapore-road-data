e5589123b501af6bc221e7c193d1d604be744897
remo5000 <vigneshshankar@gmail.com>
2018-07-13 11:09:50 +0800

    Update speed limit scraper to use requests
    Working

--- speed_limit_scraper.py
+++ speed_limit_scraper.py
@@ -1,7 +1,14 @@
-import urllib.request
+import requests
 
+# Returns speed limit of a given road name in string format.
+# Scrapes ONE.MOTORIN website
+# Invalid/failed response returns empty string
 def get_speed_limit(road_name):
     keywords = '+'.join(road_name.upper().split())
     url = f'https://www.onemotoring.com.sg/content/onemotoring/en/on_the_roads/road_safety/speed_limits.html?keyword={keywords}'
-    html = urllib.request.urlopen(url).read()
+    # html = urllib.request.urlopen(url).read()
+    response = requests.get(url)
+    if response.status_code != 200:
+        return '' 
+    text = response.text
     print(html)

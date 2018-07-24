c6c52a824a990b3d5bc252915014ea6e6d9d3e49
remo5000 <vigneshshankar@gmail.com>
2018-07-24 11:01:58 +0800

    Change main script
    - Use amap (doesnt work)
    - Use append_speed_limit
    - Use output_kd_tree

--- main.py
+++ main.py
@@ -5,18 +5,24 @@ from pathos.multiprocessing import ProcessingPool as Pool
 from road_point import RoadPoint
 from output_text import export_road_points
 from output_scatter import make_gmap
+from output_kd_tree import make_kdtree
 from speed_limit_scraper import get_speed_limit
 
-# Process a node and make it a roadpoint.
+# Process a road point and add a speed limit property.
 # Abstracted to enable concurrency for processing nodes.
+def append_speed_limit(road_point):
+    road_point.speed_limit = get_speed_limit(road_point.road_name)
+    return road_point
+
+# Converts a node to a RoadPoint, without speed_limit
+# This is done to prevent overloading in the multiprocessing
+# step with the large overpy node object.
 def node_to_RoadPoint(node):
     lat = float(node.lat)
     lon = float(node.lon)
     road_name = way.tags.get('name')
     lane_count =  way.tags.get('lanes')
-    speed_limit = get_speed_limit(road_name)
-    rp = RoadPoint(node.id, lat, lon, road_name,\
-        lane_count, speed_limit) 
+    return RoadPoint(node.id, lat, lon, road_name, lane_count) 
 
 # Get API
 api = op.Overpass()
@@ -32,14 +38,21 @@ result = api.query(query)
 all_ways = result.get_ways()
 ways = [w for w in all_ways if 'name' in w.tags and 'lanes' in w.tags]
 
+# Remove duplicates in nodes (e.g 2 connected ways sharing a node)
 node_dict = {}
 for way in ways:
     for node in way.nodes:
         node_dict[node.id] = node
 
 unique_nodes = list(node_dict.values())
-with Pool() as p:
-    road_points = p.map(node_to_RoadPoint, unique_nodes)
+road_points_without_speedlimit = [node_to_RoadPoint(n) for n in unique_nodes]
+
+# sys.setrecursionlimit(2000)
+with Pool(1) as p:
+    job = p.amap(append_speed_limit, road_points_without_speedlimit)
+    road_points = job.get()
+    print("done processing all points!")
     export_road_points(road_points)
+    kdtree = make_kd_tree(road_points)
     make_gmap(road_points)
 

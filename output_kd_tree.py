4b4f423e5a716a6f87a7643c41e24871ed940aeb
remo5000 <vigneshshankar@gmail.com>
2018-07-24 19:10:06 +0800

    Add function to load kd tree from textdump

--- output_kd_tree.py
+++ output_kd_tree.py
@@ -1,15 +1,21 @@
-from pickle import dumps
+from pickle import dumps, loads
+from ast import literal_eval
 from scipy.spatial import cKDTree as KDtree
 
 # Takes in an array of road points and 
-# Outputs a pickled kd-tree into the directory.
+# Outputs a pickled kd-tree into the directory
 def make_kdtree(road_points):
-    coords = []
-    for rp in road_points:
-        print(rp)
-        coords.append((rp.lat, rp.lon))
+    coords = [(rp.lat, rp.lon) for rp in road_points] 
     tree = KDtree(coords)
     dumped = dumps(tree)
     open("kdtree.txt", "w")
     print(dumped, file=open("kdtree.txt", "a+"))
     return tree  
+
+# Load a kd tree from kdtree.txt in the same directory.
+def load_dumped_kdtree():
+    dumped_file = open("kdtree.txt", "r")
+    dumped = literal_eval(dumped_file.read())
+    kdtree = loads(dumped)
+    return kdtree
+

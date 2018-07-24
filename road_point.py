d2a8f83421b0f4cf795782ddd6306f502d209257
remo5000 <vigneshshankar@gmail.com>
2018-07-24 11:01:23 +0800

    Make speed limit not a constructor function

--- road_point.py
+++ road_point.py
@@ -1,11 +1,11 @@
 class RoadPoint:
     # lat:float lon:float roadname:string lane_count:int
     def __init__(self, node_id, lat, lon, road_name,\
-            lane_count, speed_limit):
+            lane_count):
         self.node_id = node_id
         self.lat = lat
         self.lon = lon
         self.road_name = road_name
         self.lane_count = lane_count
-        self.speed_limit = speed_limit
+        self.speed_limit = ""
 

d7eb876e6c56688ed88d9be370614cc135637f60
remo5000 <vigneshshankar@gmail.com>
2018-08-05 12:48:56 +0800

    Add loading of dictionary in load script

--- loadkdtree.py
+++ loadkdtree.py
@@ -1,3 +1,4 @@
-from output_kd_tree import load_dumped_kdtree
+from output_kd_tree import load_dumped_dictionary, load_dumped_kdtree
 
 kdtree = load_dumped_kdtree()
+dictionary = load_dumped_dictionary()

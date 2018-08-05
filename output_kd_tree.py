accf3429727efc4b53b25adfc1cb68a7c7f89b3d
remo5000 <vigneshshankar@gmail.com>
2018-08-05 12:16:29 +0800

    Add dump for dictionary

--- output_kd_tree.py
+++ output_kd_tree.py
@@ -3,13 +3,24 @@ from ast import literal_eval
 from scipy.spatial import cKDTree as KDtree
 
 # Takes in an array of road points and 
-# Outputs a pickled kd-tree into the directory
+# Outputs pickled forms of 
+# 1) A kd-tree, with nodes as coordinates
+# 2) A dictionary, mapping coordinates (lat, lon) to RoadPoints 
 def make_kdtree(road_points):
-    coords = [(rp.lat, rp.lon) for rp in road_points] 
+    coords = []
+    dictionary = {}
+    for rp in road_points:
+        coords.append((rp.lat, rp.lon))
+        dictionary[(rp.lat, rp.lon)] = rp
     tree = KDtree(coords)
+    # Dump tree
     dumped = dumps(tree)
     open("kdtree.txt", "w")
     print(dumped, file=open("kdtree.txt", "a+"))
+    # Dump dictionary
+    dumped = dumps(dictionary)
+    open("coords_to_roadpoints.txt", "w")
+    print(dumped, file=open("coords_to_roadpoints.txt", "a+"))
     return tree  
 
 # Load a kd tree from kdtree.txt in the same directory.
